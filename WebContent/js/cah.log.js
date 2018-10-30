f305b997d2a597b8bf39dea611634b208d94455a
Benji Collins <collinsbenji13@gmail.com>
2018-10-30 14:02:27 +1030

    Fixed message and added alert

--- WebContent/js/cah.log.js
+++ WebContent/js/cah.log.js
@@ -1,26 +1,3 @@
-/*
- * Copyright (c) 2012-2018, Andy Janata
- * All rights reserved.
- * 
- * Redistribution and use in source and binary forms, with or without modification, are permitted
- * provided that the following conditions are met:
- * 
- * * Redistributions of source code must retain the above copyright notice, this list of conditions
- *   and the following disclaimer.
- * * Redistributions in binary form must reproduce the above copyright notice, this list of
- *   conditions and the following disclaimer in the documentation and/or other materials provided
- *   with the distribution.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
- * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
- * WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 /**
  * Logging functions.
  * 
@@ -102,7 +79,7 @@ cah.log.status_with_game = function(game_or_id, text, opt_class, opt_allow_html,
   } else {
     node = $("<span></span><br/>");
   }
-  var full_msg = "[" + new Date().toLocaleTimeString() + "] " + text + "\n";
+  var full_msg = text + "\n";
   if (opt_allow_html) {
     $(node[0]).html(full_msg);
   } else {
@@ -116,6 +93,12 @@ cah.log.status_with_game = function(game_or_id, text, opt_class, opt_allow_html,
   if (game_or_id !== null || opt_class) {
     cah.log.ariaStatus(text);
   }
+  $.titleAlert("New message!", {
+    requireBlur:true,
+    stopOnFocus:true,
+    duration:4700,
+    interval:700
+})
 
   if (scroll) {
     logElement.prop("scrollTop", logElement.prop("scrollHeight"));
